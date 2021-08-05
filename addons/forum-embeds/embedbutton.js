9b7fbe9cfa7d38c630a153f01cd594896b714a21
CST1229 <CST1229@users.noreply.github.com>
2021-08-05 11:44:38 +0000

    Format code

--- addons/forum-embeds/embedbutton.js
+++ addons/forum-embeds/embedbutton.js
@@ -9,66 +9,81 @@ export default async function ({ addon, global, console, safeMsg }) {
   const embedIcon =
     'url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAAXNSR0IArs4c6QAAAPhJREFUOI1jYKAQMMIY9+/f/0+KRkVFRUa4Affv3/8vKyvLMP9CA1xBokEDw+lEU4b3V54wCOrIMJjOP41iwOPHjxkUFRUZmZAFf/79zfD8y304//2VJwxGZpwM7688wekSFnSBKpuFcLagjgzDuVMQF+ACKF7Yv38/ToXIwNHREe4FDBfAgIWFBQr/xIkTWNVhNcDCwoJhxc1Ohudf7jNI8igyPP9yn6HQYgZWQ5iw6GdgYGCAB+bzL/cZhDhxhwFOL3z4+Y1BSUANp0aCBnD84WGI16ohaABWL5w4cYKh0m4ahhhJLsClAa8Bjo6ORGlCBhRnJooBAIxiVSpSDkOkAAAAAElFTkSuQmCC")'; //Add Embed icon
 
-//Constants
-//Regexes (i tried using global but it just wouldn't work for some reason)
-const scratchProjectRegExp = /^scratch.mit.edu\/projects\/\d+(?:$|\/$)|embed(?:\/?)$/;
-const youTubeRegExp = /^(?:www\.)?youtube.com\/(?:watch\/?\?v=[0-9A-Za-z-_]+|embed\/[0-9A-Za-z-_]+)/;
-const youTubeDiscussRegExp = /^scratch.mit.edu\/discuss\/youtube\/[0-9A-Za-z-_]+/;
-const audioRegExp = /(?:\.ogg|\.mp3)|(?:\.wav)$/;
-const videoRegExp = /\.mp4|\.webm$/;
-//Elements (also simultaneously making sure that the editor exists)
-const textBox = await addon.tab.waitForElement("#id_body, #id_signature"); //Post editor textbox
-const linkButton = await addon.tab.waitForElement(".markItUpButton6"); //Link button (used to insert the button after the link button)
-//Images
-const embedIcon = 'url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAAXNSR0IArs4c6QAAAPhJREFUOI1jYKAQMMIY9+/f/0+KRkVFRUa4Affv3/8vKyvLMP9CA1xBokEDw+lEU4b3V54wCOrIMJjOP41iwOPHjxkUFRUZmZAFf/79zfD8y304//2VJwxGZpwM7688wekSFnSBKpuFcLagjgzDuVMQF+ACKF7Yv38/ToXIwNHREe4FDBfAgIWFBQr/xIkTWNVhNcDCwoJhxc1Ohudf7jNI8igyPP9yn6HQYgZWQ5iw6GdgYGCAB+bzL/cZhDhxhwFOL3z4+Y1BSUANp0aCBnD84WGI16ohaABWL5w4cYKh0m4ahhhJLsClAa8Bjo6ORGlCBhRnJooBAIxiVSpSDkOkAAAAAElFTkSuQmCC")'; //Add Embed icon
+  //Constants
+  //Regexes (i tried using global but it just wouldn't work for some reason)
+  const scratchProjectRegExp = /^scratch.mit.edu\/projects\/\d+(?:$|\/$)|embed(?:\/?)$/;
+  const youTubeRegExp = /^(?:www\.)?youtube.com\/(?:watch\/?\?v=[0-9A-Za-z-_]+|embed\/[0-9A-Za-z-_]+)/;
+  const youTubeDiscussRegExp = /^scratch.mit.edu\/discuss\/youtube\/[0-9A-Za-z-_]+/;
+  const audioRegExp = /(?:\.ogg|\.mp3)|(?:\.wav)$/;
+  const videoRegExp = /\.mp4|\.webm$/;
+  //Elements (also simultaneously making sure that the editor exists)
+  const textBox = await addon.tab.waitForElement("#id_body, #id_signature"); //Post editor textbox
+  const linkButton = await addon.tab.waitForElement(".markItUpButton6"); //Link button (used to insert the button after the link button)
+  //Images
+  const embedIcon =
+    'url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAAXNSR0IArs4c6QAAAPhJREFUOI1jYKAQMMIY9+/f/0+KRkVFRUa4Affv3/8vKyvLMP9CA1xBokEDw+lEU4b3V54wCOrIMJjOP41iwOPHjxkUFRUZmZAFf/79zfD8y304//2VJwxGZpwM7688wekSFnSBKpuFcLagjgzDuVMQF+ACKF7Yv38/ToXIwNHREe4FDBfAgIWFBQr/xIkTWNVhNcDCwoJhxc1Ohudf7jNI8igyPP9yn6HQYgZWQ5iw6GdgYGCAB+bzL/cZhDhxhwFOL3z4+Y1BSUANp0aCBnD84WGI16ohaABWL5w4cYKh0m4ahhhJLsClAa8Bjo6ORGlCBhRnJooBAIxiVSpSDkOkAAAAAElFTkSuQmCC")'; //Add Embed icon
 
-//Add the button
-var embedButton = document.createElement("li");
-embedButton.className = "markItUpButton markItUpButton17";
-//The <a> element
-var embedButtonLink = document.createElement("a");
-embedButtonLink.textContent = safeMsg("embed-add-hover");
-embedButtonLink.title = safeMsg("embed-add-hover");
-embedButtonLink.style["background-image"] = embedIcon;
-embedButtonLink.id = "embedButton";
-embedButton.appendChild(embedButtonLink);
-//Insert the button
-linkButton.parentNode.insertBefore(embedButton, linkButton.nextSibling);
-//Dynamic enable and disable
-addon.tab.displayNoneWhileDisabled(embedButton, { display: "inline" });
+  //Add the button
+  var embedButton = document.createElement("li");
+  embedButton.className = "markItUpButton markItUpButton17";
+  //The <a> element
+  var embedButtonLink = document.createElement("a");
+  embedButtonLink.textContent = safeMsg("embed-add-hover");
+  embedButtonLink.title = safeMsg("embed-add-hover");
+  embedButtonLink.style["background-image"] = embedIcon;
+  embedButtonLink.id = "embedButton";
+  embedButton.appendChild(embedButtonLink);
+  //Insert the button
+  linkButton.parentNode.insertBefore(embedButton, linkButton.nextSibling);
+  //Dynamic enable and disable
+  addon.tab.displayNoneWhileDisabled(embedButton, { display: "inline" });
 
-//Button function
-const addEmbedFunction = function() {
-	var link = prompt(safeMsg("embed-add-prompt"))
-	if (link == null) {return;} //Cancelling
-	
-	const invalidURL = function() {alert(safeMsg("embed-add-error"));}; //If a URL is invalid
-	try {
-		var url = new URL(link); //Validate the URL
-	} catch(error) {
-		try {
-			var url = new URL("https://" + link); //Since URL() doesn't like URLs without protocols, we have to do this
-		} catch(error) {
-			invalidURL(); //Okay, the URL is actually invalid
-			return;
-		}
-	}
-	if (scratchProjectRegExp.test(url.hostname + url.pathname) || youTubeRegExp.test(url.hostname + url.pathname + url.search) || youTubeDiscussRegExp.test(url.hostname + url.pathname) || audioRegExp.test(url.pathname) || videoRegExp.test(url.pathname)) { //Test for any of the regexes
-		//It's a valid link for embeds
-		var insertText = "[url=ex-embed][/url][url=" + url + "]" + safeMsg("embed-placeholder") + "[/url]"; //Prepare the text to add to the textbox
-		if (textBox.selectionStart === textBox.value.length && textBox.selectionEnd === textBox.value.length) { //Cursor is at the end
-			textBox.value += insertText;
-		} else { //Cursor is somewhere else or is selecting a part of the textbox
-			textBox.value = textBox.value.substring(0, textBox.selectionStart)
-			+ insertText
-			+ textBox.value.substring(textBox.selectionEnd - 1, textBox.value.length);
-		}
-	} else { //Not an embed link
-		invalidURL();
-		return;
-	}
-}
-//Finally, add all this to the embed button
-embedButtonLink.onclick = addEmbedFunction;
+  //Button function
+  const addEmbedFunction = function () {
+    var link = prompt(safeMsg("embed-add-prompt"));
+    if (link == null) {
+      return;
+    } //Cancelling
 
+    const invalidURL = function () {
+      alert(safeMsg("embed-add-error"));
+    }; //If a URL is invalid
+    try {
+      var url = new URL(link); //Validate the URL
+    } catch (error) {
+      try {
+        var url = new URL("https://" + link); //Since URL() doesn't like URLs without protocols, we have to do this
+      } catch (error) {
+        invalidURL(); //Okay, the URL is actually invalid
+        return;
+      }
+    }
+    if (
+      scratchProjectRegExp.test(url.hostname + url.pathname) ||
+      youTubeRegExp.test(url.hostname + url.pathname + url.search) ||
+      youTubeDiscussRegExp.test(url.hostname + url.pathname) ||
+      audioRegExp.test(url.pathname) ||
+      videoRegExp.test(url.pathname)
+    ) {
+      //Test for any of the regexes
+      //It's a valid link for embeds
+      var insertText = "[url=ex-embed][/url][url=" + url + "]" + safeMsg("embed-placeholder") + "[/url]"; //Prepare the text to add to the textbox
+      if (textBox.selectionStart === textBox.value.length && textBox.selectionEnd === textBox.value.length) {
+        //Cursor is at the end
+        textBox.value += insertText;
+      } else {
+        //Cursor is somewhere else or is selecting a part of the textbox
+        textBox.value =
+          textBox.value.substring(0, textBox.selectionStart) +
+          insertText +
+          textBox.value.substring(textBox.selectionEnd - 1, textBox.value.length);
+      }
+    } else {
+      //Not an embed link
+      invalidURL();
+      return;
+    }
+  };
+  //Finally, add all this to the embed button
+  embedButtonLink.onclick = addEmbedFunction;
 }
--- addons/forum-embeds/embeds.js
+++ addons/forum-embeds/embeds.js
@@ -1,81 +1,89 @@
 export default async function ({ addon, global, console, safeMsg }) {
+  //Creates the embeds themselves from embed links.
 
-//Creates the embeds themselves from embed links.
+  //Constants
+  //Regexes
+  const scratchProjectRegExp = /^scratch.mit.edu\/projects\/\d+(?:$|\/$)|embed(?:\/?)$/;
+  const youTubeRegExp = /^(?:www\.)?youtube.com\/(?:watch\/?\?v=[0-9A-Za-z-_]+|embed\/[0-9A-Za-z-_]+)/;
+  const youTubeDiscussRegExp = /^scratch.mit.edu\/discuss\/youtube\/[0-9A-Za-z-_]+/;
+  const audioRegExp = /(?:\.ogg|\.mp3)|(?:\.wav)$/;
+  const videoRegExp = /\.mp4|\.webm$/;
 
-//Constants
-//Regexes
-const scratchProjectRegExp = /^scratch.mit.edu\/projects\/\d+(?:$|\/$)|embed(?:\/?)$/;
-const youTubeRegExp = /^(?:www\.)?youtube.com\/(?:watch\/?\?v=[0-9A-Za-z-_]+|embed\/[0-9A-Za-z-_]+)/;
-const youTubeDiscussRegExp = /^scratch.mit.edu\/discuss\/youtube\/[0-9A-Za-z-_]+/;
-const audioRegExp = /(?:\.ogg|\.mp3)|(?:\.wav)$/;
-const videoRegExp = /\.mp4|\.webm$/;
+  //Loop
+  while (true) {
+    //Get all embed urls
+    var embedLink = await addon.tab.waitForElement(
+      '.postmsg a[href="http://ex-embed"]+a, .postsignature a[href="http://ex-embed"]+a',
+      {
+        markAsSeen: true,
+      }
+    );
+    var url = new URL(embedLink.href); //URL of embed
+    var embedElement; //Embed element
+    var embedded = false; //If the embed creation is successful
 
-//Loop
-while (true) {
-	//Get all embed urls
-	var embedLink = await addon.tab.waitForElement('.postmsg a[href="http://ex-embed"]+a, .postsignature a[href="http://ex-embed"]+a', {
-		markAsSeen: true,
-	});
-	var url = new URL(embedLink.href); //URL of embed
-	var embedElement; //Embed element
-	var embedded = false; //If the embed creation is successful
-	
-	//Embed types
-	//Scratch project
-	if (scratchProjectRegExp.test(url.hostname + url.pathname)) {
-		embedElement = document.createElement("iframe");
-		embedElement.title = safeMsg("scratch-title");
-		embedElement.src = "https://scratch.mit.edu/projects/" + url.pathname.match(/\d+/g)[0] + "/embed/";
-		embedElement.width = 499;
-		embedElement.height = 416;
-		
-		embedded = true;
-	}
-	//Youtube video
-	if (youTubeRegExp.test(url.hostname + url.pathname + url.search) || youTubeDiscussRegExp.test(url.hostname + url.pathname)) {
-		embedElement = document.createElement("iframe");
-		embedElement.title = safeMsg("youtube-title");
-		embedElement.src = "https://youtube.com/embed/" + (url.pathname + url.search).match(/(?:(?:embed\/|tch\?v=)|utube\/)[0-9A-Za-z-_]+/g)[0].substring(6) + "/?showinfo=0&rel=0";
-		embedElement.width = 560;
-		embedElement.height = 315;
-		
-		embedded = true;
-	}
-	//Audio file (ogg, mp3, wav)
-	if (audioRegExp.test(url.pathname)) {
-		embedElement = document.createElement("audio");
-		embedElement.title = safeMsg("audio-title");
-		embedElement.setAttribute("controls", "");
-		embedElement.style.width = "calc(100% - 4px)";
-		//<source> element
-		var sourceElement = document.createElement("source");
-		sourceElement.src = url.href;
-		embedElement.appendChild(sourceElement);
-		
-		embedded = true;
-	}
-	//Video file (mp4, webm)
-	if (videoRegExp.test(url.pathname)) {
-		embedElement = document.createElement("video");
-		embedElement.title = safeMsg("video-title");
-		embedElement.setAttribute("controls", "");
-		embedElement.style.width = "calc(100% - 4px)";
-		//<source> element
-		var sourceElement = document.createElement("source");
-		sourceElement.src = url.href;
-		embedElement.appendChild(sourceElement);
-		
-		embedded = true;
-	}
-	if (embedded) { //If the embedding is successful
-		//Insert the embed
-		embedElement.style.border = "solid 2px #e0e0e0";
-		embedElement.style.display = "block";
-		embedLink.parentNode.insertBefore(embedElement, embedLink);
-		//Dynamic enable and disable
-		addon.tab.displayNoneWhileDisabled(embedElement, { display: "inline" });
-		embedLink.classList.add("sa-embed-link");
-	}
-}
+    //Embed types
+    //Scratch project
+    if (scratchProjectRegExp.test(url.hostname + url.pathname)) {
+      embedElement = document.createElement("iframe");
+      embedElement.title = safeMsg("scratch-title");
+      embedElement.src = "https://scratch.mit.edu/projects/" + url.pathname.match(/\d+/g)[0] + "/embed/";
+      embedElement.width = 499;
+      embedElement.height = 416;
 
+      embedded = true;
+    }
+    //Youtube video
+    if (
+      youTubeRegExp.test(url.hostname + url.pathname + url.search) ||
+      youTubeDiscussRegExp.test(url.hostname + url.pathname)
+    ) {
+      embedElement = document.createElement("iframe");
+      embedElement.title = safeMsg("youtube-title");
+      embedElement.src =
+        "https://youtube.com/embed/" +
+        (url.pathname + url.search).match(/(?:(?:embed\/|tch\?v=)|utube\/)[0-9A-Za-z-_]+/g)[0].substring(6) +
+        "/?showinfo=0&rel=0";
+      embedElement.width = 560;
+      embedElement.height = 315;
+
+      embedded = true;
+    }
+    //Audio file (ogg, mp3, wav)
+    if (audioRegExp.test(url.pathname)) {
+      embedElement = document.createElement("audio");
+      embedElement.title = safeMsg("audio-title");
+      embedElement.setAttribute("controls", "");
+      embedElement.style.width = "calc(100% - 4px)";
+      //<source> element
+      var sourceElement = document.createElement("source");
+      sourceElement.src = url.href;
+      embedElement.appendChild(sourceElement);
+
+      embedded = true;
+    }
+    //Video file (mp4, webm)
+    if (videoRegExp.test(url.pathname)) {
+      embedElement = document.createElement("video");
+      embedElement.title = safeMsg("video-title");
+      embedElement.setAttribute("controls", "");
+      embedElement.style.width = "calc(100% - 4px)";
+      //<source> element
+      var sourceElement = document.createElement("source");
+      sourceElement.src = url.href;
+      embedElement.appendChild(sourceElement);
+
+      embedded = true;
+    }
+    if (embedded) {
+      //If the embedding is successful
+      //Insert the embed
+      embedElement.style.border = "solid 2px #e0e0e0";
+      embedElement.style.display = "block";
+      embedLink.parentNode.insertBefore(embedElement, embedLink);
+      //Dynamic enable and disable
+      addon.tab.displayNoneWhileDisabled(embedElement, { display: "inline" });
+      embedLink.classList.add("sa-embed-link");
+    }
+  }
 }
